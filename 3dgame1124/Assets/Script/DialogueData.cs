d4a2b3367f85f73d738e0595acf4c2cba55a84fb
WUwilliomggad0708 <wuwilliomggad@gmail.com>
2022-12-22 15:45:38 +0800

    1222 Dialogue data

---
+++ 3dgame1124/Assets/Script/DialogueData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace xin
+{ 
+    /// <summary>
+    /// 對話資料
+    /// </summary>
+    [CreateAssetMenu(menuName = "xin/Dialogue Data",fileName = "New Dialogue Data")]
+    public class DialogueData : ScriptableObject
+    {
+        [Header("對話者名稱")]
+        public string dialogueName;
+        [Header("對話內容"),TextArea(2,5)]
+        public string[] dialogueContents;
+
+    }
+}
